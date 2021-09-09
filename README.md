ed8beb29689207c6973a9a394a33d4e1ecdecf10
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-09-10 00:03:16 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+#### Desktop Theme 1
+![](./img/desktop-theme-1.png)
+
+#### Mobile Portrait Theme 1
+![](./img/mobile-portrait-theme-1.png)
+
+#### Mobile Rotated Theme 1
+![](./img/mobile-rotated-theme-1.png)
+
+- I you want to see other themes, see the `img` folder
+- Visit the application [here](https://usmonbek-umarbekovich.github.io/Calculator-App-Main/)
+
+
+### Links
+
+- Solution URL: [Solution](https://github.com/usmonbek-umarbekovich/Calculator-App-Main)
+- Live Site URL: [Live Site](https://usmonbek-umarbekovich.github.io/Calculator-App-Main/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Vanilla JavaScript
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+## Author
+
+- GitHub - [Usmonbek Rustamov](https://github.com/usmonbek-umarbekovich)
+- Frontend Mentor - [@usmonbek-umarbekovich](https://www.frontendmentor.io/profile/usmonbek-umarbekovich)
---
+++ src/script.js
@@ -0,0 +1,304 @@
+import '@babel/polyfill';
+
+// Elements
+const keyContainer = document.querySelector('.key-items');
+const toggleBar = document.querySelector('.toggle-bar');
+const content = document.querySelector('.content');
+const error = document.querySelector('.error');
+
+class Calculator {
+  #contentWidth;
+  #currOperation = null;
+  #currNumber = null;
+  #prevNumber = null;
+  #numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  #others = {
+    dot: '.',
+    eq: '=',
+    plus: '+',
+    min: '-',
+    mul: 'x',
+    div: '/',
+    del: 'del',
+    res: 'reset',
+  };
+  #operations = {
+    plus(a, b) {
+      return a + b;
+    },
+    min(a, b) {
+      return a - b;
+    },
+    mul(a, b) {
+      return a * b;
+    },
+    div(a, b) {
+      return a / b;
+    },
+  };
+
+  constructor() {
+    this.#generateMarkup();
+    this.#contentWidth = content.scrollWidth;
+    toggleBar.addEventListener('click', this.#switchThemes);
+    keyContainer.addEventListener('click', this.#handleKeys.bind(this));
+    window.addEventListener('keydown', this.#bindKeyboard.bind(this));
+  }
+
+  #insertMarkup(type, dataValue, gridArea, value) {
+    const markup = `
+      <li
+        class="small-key ${type}"
+        data-value="${dataValue}"
+        style="grid-area: ${gridArea}"
+      >
+        ${value}
+      </li>`;
+    keyContainer.insertAdjacentHTML('beforeend', markup);
+  }
+
+  #generateMarkup() {
+    this.#numbers.forEach(n => this.#insertMarkup('number', n, `n-${n}`, n));
+    Object.entries(this.#others).forEach(val =>
+      this.#insertMarkup(val[0], val[0], val[0], val[1])
+    );
+    content.focus();
+  }
+
+  #switchThemes(e) {
+    const radio = e.target.closest('input');
+    if (!radio) return;
+    document.body.className = `theme-${radio.value}`;
+  }
+
+  #renderError(msg) {
+    error.textContent = '';
+    error.textContent = msg;
+    error.classList.remove('hidden');
+    setTimeout(function () {
+      error.classList.add('hidden');
+    }, 1500);
+  }
+
+  #setplaceholder() {
+    const prevNumber = this.#addCommas(this.#prevNumber.toString());
+    const number =
+    prevNumber.length > 18 ? this.#prevNumber.toExponential(3) : prevNumber;
+    content.value = number + 'over';
+
+    if (content.scrollWidth > this.#contentWidth) {
+      content.style.fontSize = '1.5rem';
+    } else {
+      content.style.fontSize = '2rem';
+    }
+
+    content.value = '';
+    content.setAttribute('placeholder', number);
+    
+  }
+
+  #remove() {
+    if (
+      content.value.length === 0 &&
+      content.getAttribute('placeholder').length === 0
+    ) {
+      return;
+    }
+
+    let number;
+    if (content.value.length === 0) {
+      number = this.#removeCommas(content.getAttribute('placeholder'));
+      content.setAttribute('placeholder', '');
+    }
+
+    if (content.value.length > 0) {
+      number = this.#removeCommas(content.value);
+    }
+
+    const deleted = number.toString().slice(0, -1);
+    if (deleted.length === 0) {
+      this.#clearScreen();
+    }
+
+    if (deleted.length > 0) {
+      content.value = this.#addCommas(deleted) + 'overf';
+      
+      if (content.scrollWidth === this.#contentWidth) {
+        content.style.fontSize = '2rem';
+      }
+
+      content.value = this.#addCommas(deleted);
+
+      if (this.#prevNumber !== null && this.#currNumber !== null) {
+        this.#setNull();
+      }
+    }
+  }
+
+  #clearScreen() {
+    content.value = '';
+    content.setAttribute('placeholder', '');
+  }
+
+  #setNull() {
+    this.#currNumber = null;
+    this.#prevNumber = null;
+    this.#currOperation = null;
+  }
+
+  #addCommas(number) {
+    let [integer, decimal] = number.replaceAll(',', '').split('.');
+    decimal = decimal ? `.${decimal}` : '';
+    return Number(integer).toLocaleString('en') + decimal;
+  }
+
+  #removeCommas(number) {
+    if (number === '.') return 0.0;
+    return Number(number.replaceAll(',', ''));
+  }
+
+  #addContent(value) {
+    if (content.value.length === 18)
+      return this.#renderError('You have reached the maximum');
+
+    if (value === '.') {
+      content.value += '.';
+    } else {
+      const number = content.value + value;
+      content.value = this.#addCommas(number);
+
+      if (content.scrollWidth > this.#contentWidth) {
+        content.style.fontSize = '1.5rem';
+      } else {
+        content.style.fontSize = '2rem';
+      }
+    }
+  }
+
+  #doOperation(type) {
+    this.#currNumber =
+      content.value.length > 0
+        ? this.#removeCommas(content.value)
+        : this.#currNumber;
+    this.#prevNumber = Number(
+      this.#operations[type](this.#prevNumber, this.#currNumber).toFixed(8)
+    );
+
+    this.#setplaceholder();
+  }
+
+  #handleOperation(operationType) {
+    if (
+      this.#prevNumber !== null &&
+      this.#currNumber === null &&
+      content.value.length > 0
+    ) {
+      this.#doOperation(this.#currOperation);
+      this.#currOperation = operationType;
+    }
+
+    if (
+      (this.#prevNumber === null && content.value.length > 0) ||
+      content.getAttribute('placeholder').length > 0
+    ) {
+      this.#prevNumber =
+        content.value.length > 0
+          ? this.#removeCommas(content.value)
+          : this.#prevNumber;
+      this.#currNumber = null;
+      this.#currOperation = operationType;
+    }
+
+    if (content.value.length > 0) {
+      this.#setplaceholder();
+    }
+  }
+
+  #handleKeys(e) {
+    const key = e.target.closest('li');
+    if (!key) return;
+
+    if (
+      key === e.target.closest('.number') ||
+      (key.dataset.value === 'dot' && !content.value.includes('.'))
+    ) {
+      const value = key.dataset.value === 'dot' ? '.' : key.dataset.value;
+      this.#addContent(value);
+    }
+
+    if (
+      key.dataset.value === 'min' &&
+      content.value.length === 0 &&
+      content.getAttribute('placeholder').length === 0
+    ) {
+      content.value = '-';
+    }
+
+    if (key.dataset.value === 'plus' && content.value === '-') {
+      content.value = '';
+    }
+
+    if (key.dataset.value === 'del') {
+      this.#remove();
+    }
+
+    if (key.dataset.value === 'res') {
+      this.#clearScreen();
+    }
+
+    if (content.value === '.' || content.value === '-') return;
+
+    if (
+      key.dataset.value === 'eq' &&
+      this.#currOperation &&
+      (this.#currNumber !== null || content.value.length > 0)
+    ) {
+      this.#doOperation(this.#currOperation);
+    }
+
+    // Operations: +, -, x, /
+    if (Object.keys(this.#operations).includes(key.dataset.value)) {
+      this.#handleOperation(key.dataset.value);
+    }
+
+    if (
+      content.value.length === 0 &&
+      content.getAttribute('placeholder').length === 0
+    ) {
+      this.#setNull();
+    }
+  }
+
+  #bindKeyboard(e) {
+    if (Number.isInteger(+e.key) || e.key === '.') {
+      this.#addContent(e.key);
+    }
+
+    if (e.key === 'Backspace') {
+      this.#remove();
+    }
+
+    if (e.key === 'Delete') {
+      this.#clearScreen();
+      this.#setNull();
+    }
+
+    if (
+      e.key === 'Enter' &&
+      this.#currOperation &&
+      (this.#currNumber !== null || content.value.length > 0)
+    ) {
+      this.#doOperation(this.#currOperation);
+    }
+
+    if (['+', '-', '*', '/'].includes(e.key)) {
+      let operationType = Object.entries(this.#others).find(
+        entry => entry[1] === e.key
+      )?.[0];
+      operationType = operationType ?? 'mul';
+      this.#handleOperation(operationType);
+    }
+  }
+}
+
+new Calculator();
